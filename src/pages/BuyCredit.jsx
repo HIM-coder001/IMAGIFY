6a79cbd475a18d2dc2dedb71a81a80cfe1280201
HIM <josekamaemaina2005@gmail.com>
2026-06-01 16:32:42 +0300

    Animations with framer motion

--- src/pages/BuyCredit.jsx
+++ src/pages/BuyCredit.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import {assets, plans } from '../assets/assets'
 import { AppContext } from '../context/AppContext'
+import {motion} from 'framer-motion'
 
 const BuyCredit = () => {
 
@@ -8,7 +9,13 @@ const BuyCredit = () => {
 
 
   return (
-    <div className='min-h-[80vh] text-center pt-14 mb-10'>
+    <motion.div
+      initial={{ opacity: 0.2, y: 100 }}
+      transition={{ duration: 1 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      className='min-h-[80vh] text-center pt-14 mb-10'
+    >
        <button className='border border-gray-400 px-10 py02 rounded-full mb-6'>Our Plan</button>
        <h1 className='text-center text-3xl font-medium mb-6 sm:mb-10' >Choose the item</h1>
 
@@ -29,7 +36,7 @@ const BuyCredit = () => {
           </div>
         ))}
        </div>
-    </div>
+    </motion.div>
   )
 }
 
