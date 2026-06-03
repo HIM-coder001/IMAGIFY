3582ce590610ea0f0b8dacc9c797b89a08417e3a
HIM <josekamaemaina2005@gmail.com>
2026-06-03 16:57:55 +0300

    feat: setup login form with useState and controlled inputs

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,12 +2,42 @@ import React, { useContext, useEffect, useState } from 'react'
 import {assets} from '../assets/assets'
 import { AppContext } from '../context/AppContext'
 import {motion} from 'framer-motion'
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const Login = () => {
 
      const [state , setState] = useState('Login')
-     const {setShowLogin} = useContext(AppContext)
-      
+     const {setShowLogin , backendUrl , setToken ,setUser} = useContext(AppContext)
+
+     const [name,setName] = useState("")  
+     const [email,setEmail] = useState("")  
+     const [password,setPassword] = useState("")  
+
+     const onSubmitHandler = async (e) => {
+        e.preventDefault();
+        try {
+
+            if(state === 'Login'){
+               const {data} = await axios.post(backendUrl + '/api/user/login' , {email , password})
+
+               if(data.success){
+                   setToken(data.token)
+                   setUser(data.user)
+                   localStorage.setItem('token' , data.token)
+                   setShowLogin(false)
+                }
+                else{
+                   toast.error(data.message)
+                }
+
+            }
+
+        } catch (error) {
+            
+        }
+      }
+
      useEffect(()=>{
         document.body.style.overflow = 'hidden';
         return ()=>{
@@ -22,28 +52,28 @@ const Login = () => {
     whileInView={{opacity:1 , y:0}}
     viewport={{once:true}}
         className='fixed top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
-        <form className='relative bg-white p-10 rounded-xl text-slate-500'>
+        <form onSubmit={onSubmitHandler} className='relative bg-white p-10 rounded-xl text-slate-500'>
             <h1 className='text-center text-2xl text-neutral-700 font-medium'>{state}</h1>
             <p className='text-sm'>Welcome back please sign in to contine</p>
 
             {state !== 'Login' && <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
                 <img src={assets.profile_icon} alt="" className='h-6'/>
-                <input type="text" placeholder='Full name ' required className='outline-none text-sm'/>
+                <input onChange={e => setName(e.target.value)} value={name} type="text" placeholder='Full name ' required className='outline-none text-sm'/>
             </div>}
 
             <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
                 <img src={assets.email_icon} alt=""  />
-                <input type="email" placeholder='Email ' required className='outline-none text-sm'/>
+                <input onChange={e => setEmail(e.target.value)} value={email}  type="email" placeholder='Email ' required className='outline-none text-sm'/>
             </div>
 
             <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
                 <img src={assets.lock_icon} alt=""  />
-                <input type="password" placeholder='Enter your Password' required className='outline-none text-sm'/>
+                <input onChange={e => setPassword(e.target.value)} value={password}  type="password" placeholder='Enter your Password' required className='outline-none text-sm'/>
             </div>
 
             {state !== 'Login' && <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
                 <img src={assets.lock_icon} alt=""  />
-                <input type="password" placeholder='Confirm Password' required className='outline-none text-sm'/>
+                <input onChange={e => setPassword(e.target.value)} value={password}  type="password" placeholder='Confirm Password' required className='outline-none text-sm'/>
             </div>}
  
                 <p className='text-sm text-blue-600 my-4'>
